4ceb727123611ce17dba83ae763ad617000e8a8f
Slava Dobrov <dobrov.slava@yandex.ru>
2023-02-04 19:33:59 +0300

    SelectSort is ready for testing

--- src/main/java/Sorts/TestingSorts.java
+++ src/main/java/Sorts/TestingSorts.java
@@ -4,7 +4,8 @@ public class TestingSorts {
 
     public static void main(String[] args) {
         int[] array = new int[]{4,5,7,3,1,8,0,6,2,9};
-        BubbleSort.bubbleSort(array);
+//        BubbleSort.bubbleSort(array);
+        SelectSort.sort(array);
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
